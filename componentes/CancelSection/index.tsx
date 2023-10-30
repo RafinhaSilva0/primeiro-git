4fc1fee2a56bbb11bf7a0a0b5aff72449e3d1123
Rafael silva <https://github.com/RafinhaSilva0?tab=repositories>
2023-10-30 10:52:11 -0300

    projeto git

--- componentes/CancelSection/index.tsx
+++ componentes/CancelSection/index.tsx
@@ -14,7 +14,7 @@ export default function CancelSection() {
                 Cancele<span> Quando quiser! </span>
             </h3>
             <p>
-                O Rei dos Sites isenta o seu risco removendo a multa.{" "}
+                A RUN isenta o seu risco removendo a multa.{" "}
                 <span>Se por algum motivo fora do comum</span>, você não estiver
                 satisfeito com o trabalho, você pode{" "}
                 <span>cancelar quando quiser sem pagar um centavo</span> sequer pela
